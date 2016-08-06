670fcdbe1be7ce46ad357dc11b1566f47726e00f
Nantha Harish JR <harish-rajendran@users.noreply.github.com>
2016-08-06 16:24:10 +0530

    Add files via upload

---
+++ vowel or consonant.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     06-08-2016
+# Copyright:   (c) Admin 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    c=raw_input("enter the character")
+    if(c=="a" or c=="e"or c=="i"or c=="o"or c=="u"):
+        print "the character is vowel"
+    else:
+        print "the character is consonant"
+
+if __name__ == '__main__':
+    main()
