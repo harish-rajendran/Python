3b1f9531bb7bd8ca36fa971b07f825de78726d49
Nantha Harish JR <harish-rajendran@users.noreply.github.com>
2016-08-06 15:39:00 +0530

    Add files via upload

---
+++ posORneg.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     06-08-2016
+# Copyright:   (c) Admin 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    num=input("Enter the number")
+    if(num>=0):
+        print num ,"IS POSITIVE"
+    else:
+        print num ,"IS NEGATIVE"
+
+if __name__ == '__main__':
+    main()
